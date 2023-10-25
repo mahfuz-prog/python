4c8cedb3b383a4d7f1fa9278b805bd1abdbe4a7e
Mahafuzur Rahaman <62833292+mahfuz-prog@users.noreply.github.com>
2023-10-25 13:58:35 +0600

    Update run.py

--- HEIC to png converter/run.py
+++ HEIC to png converter/run.py
@@ -1,25 +1,24 @@
 import os
-import sys
 import time
 from PIL import Image
 from pillow_heif import register_heif_opener
 
 register_heif_opener()
 
-def heic_to_png():
-    input_path = os.getcwd()+ '\img'
-    output_path = os.getcwd() + '\Output Image'
+def heic_to_png(working_dir):
+    input_path = working_dir + '\img'
+    output_path = working_dir + '\Output Image'
     os.chdir(input_path)
     total_image = len(os.listdir())
     print(f'Toatal {total_image} Images')
 
-    for i in os.listdir():
+    for img in os.listdir():
         start_time = time.time()
-        name, _ = os.path.splitext(i)
-        image = Image.open(i)
+        name, _ = os.path.splitext(img)
+        image = Image.open(img)
         image.save(f'{output_path}\{name}.png')
         run_time = time.time() - start_time
         print(f'{name} processed in {run_time}')
 
 if __name__ == '__main__':
-    heic_to_png()
+    heic_to_png(os.getcwd())
