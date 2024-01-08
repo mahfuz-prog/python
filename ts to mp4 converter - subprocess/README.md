ecaf7d753812ec0349297847e2ee4f4cc4cebe1f
Mahfuz <mahfuzurrahman4414@gmail.com>
2024-01-08 22:27:35 +0600

    subprocess

---
+++ ts to mp4 converter - subprocess/README.md
@@ -0,0 +1,2 @@
+# convert .ts to .mp4 - subprocess
+#### [Download video from stream m3u8-ts](https://github.com/mahfuz-prog/python/tree/main/Download%20video%20from%20stream%20m3u8-ts) from this program i have got 271 .ts file and in this program i have changed title and converted into .mp4
---
+++ ts to mp4 converter - subprocess/converter.py
@@ -0,0 +1,45 @@
+import os
+import time
+import subprocess
+
+def ts_to_mp4(ts_list):
+	for ts in ts_list:
+		print()
+		mp4_title = ts.replace('.ts', '.mp4')
+		print(ts, ', converting to .mp4...')
+		# convert to .mp4
+		subprocess.run(['ffmpeg', '-i', ts, mp4_title], shell=True)
+		os.remove(ts)
+		print()
+		print('Program sleeping for 2 minutes.....')
+		# to reduce cpu pressure
+		time.sleep(120)
+
+# Rename the title and change the order number
+def rename(title, i):
+	title = title.split('-')
+	title[0] = i
+	title = ' '.join([str(elem) for elem in title])
+	return title
+
+
+# put all the section inside videos folder
+os.chdir('videos')
+for section in os.listdir():
+	print('section', section)
+	os.chdir(section)
+	ts_list = []
+	i = 1
+	for video in os.listdir():
+
+		# run this function once to rename title
+		# title = rename(video, i)
+		# print(title)
+		# os.rename(video, title)
+		ts_list.append(video)
+		i+=1
+
+	ts_to_mp4(ts_list)
+	ts_list = []
+	print()
+	os.chdir('..')
